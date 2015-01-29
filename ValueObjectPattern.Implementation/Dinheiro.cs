2cf555ad7edbbe2c865651ed567d93364b9a547b
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-01-29 00:54:42 -0300

    commit inicial com dinheiro implementado com testes passando

---
+++ ValueObjectPattern.Implementation/Dinheiro.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ValueObjectPattern.Implementation
+{
+    /// <summary>
+    /// Classe pode ser incrementada como por exemplo implementar outros operators, utilizar exceções customizadas e 
+    /// implementar outras operações com Dinheiro além do 'SomarCom' mas o código atual já demonstra o que é um VO
+    /// </summary>
+    public class Dinheiro
+    {
+        public string Moeda { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public Dinheiro(string moeda, decimal valor)
+        {
+            if (string.IsNullOrWhiteSpace(moeda)) throw new ArgumentNullException("moeda");
+            if (valor <= 0) throw new ArgumentException("Valor deve ser maior que zero", "valor");
+            Moeda = moeda;
+            Valor = valor;
+        }
+
+        public Dinheiro SomarCom(Dinheiro outroDinheiro)
+        {
+            if (Moeda != outroDinheiro.Moeda) throw new InvalidOperationException("Não é possível somar valores de moedas diferentes");
+            return new Dinheiro(outroDinheiro.Moeda, Valor + outroDinheiro.Valor);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            var dinheiro = obj as Dinheiro;
+            if (ReferenceEquals(null, dinheiro)) return false;
+            return (Moeda == dinheiro.Moeda && Valor == dinheiro.Valor);
+        }
+
+        public override int GetHashCode()
+        {
+            return Moeda.GetHashCode() ^ Valor.GetHashCode();
+        }
+
+        public static bool operator ==(Dinheiro dinheiro, Dinheiro outroDinheiro)
+        {
+            return Equals(dinheiro, outroDinheiro);
+        }
+
+        public static bool operator !=(Dinheiro dinheiro, Dinheiro outroDinheiro)
+        {
+            return !Equals(dinheiro, outroDinheiro);
+        }
+    }
+}
---
+++ ValueObjectPattern.Tests/DinheiroTeste.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using ValueObjectPattern.Implementation;
+
+namespace ValueObjectPattern.Tests
+{
+    [TestFixture]
+    public class DinheiroTeste
+    {
+        private Dinheiro _dinheiro, _mesmoDinheiro, _dinheiroDiferente;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dinheiro = new Dinheiro("R$", 10.00m);
+            _mesmoDinheiro = new Dinheiro("R$", 10.00m);
+            _dinheiroDiferente = new Dinheiro("US$", 10.01m);
+        }
+
+        [Test]
+        public void Deve_comparar_valores()
+        {
+            Assert.IsTrue(_dinheiro.Equals(_mesmoDinheiro));
+            Assert.IsFalse(_dinheiro.Equals(_dinheiroDiferente));
+        }
+
+        [Test]
+        public void Deve_comparar_hashcodes()
+        {
+            Assert.IsTrue(_dinheiro.GetHashCode() == _mesmoDinheiro.GetHashCode());
+            Assert.IsFalse(_dinheiro.GetHashCode() == _dinheiroDiferente.GetHashCode());
+        }
+
+        [Test]
+        public void Deve_comparar_usando_operadores_igual_e_diferente()
+        {
+            Assert.IsTrue(_dinheiro == _mesmoDinheiro);
+            Assert.IsTrue(_dinheiro != _dinheiroDiferente);
+        }
+
+        [Test]
+        public void Deve_somar_dois_valores_de_mesma_moeda()
+        {
+            var novoDinheiro = _dinheiro.SomarCom(_mesmoDinheiro);
+
+            Assert.AreEqual(new Dinheiro("R$", 20.00m), novoDinheiro);
+        }
+
+        [Test]
+        public void Nao_deve_criar_sem_informar_moeda()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Dinheiro("", 10m));
+        }
+
+        [Test]
+        public void Nao_deve_criar_com_valor_menor_igual_zero()
+        {
+            Assert.Throws<ArgumentException>(() => new Dinheiro("R$", 0m));
+        }
+
+        [Test]
+        public void Nao_deve_permitir_somar_dois_valores_de_moedas_diferentes()
+        {
+            Assert.Throws<InvalidOperationException>(() => _dinheiro.SomarCom(_dinheiroDiferente));
+        }
+    }
+}
